d1b8e318055d8cbf6a5846bcc1b312eb931988ae
filipa-mn <filipa.amn1@gmail.com>
2023-09-27 19:13:11 +0100

    v4

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('test v2')
+import requests
+import matplotlib.pyplot as plt
+
+x = requests.get("https://www.google.com/")
+
+print(dict(x.headers)['Date']) if x.status_code == 200 else print("Sem acesso")
+
+y = requests.get("https://www.sapo.pt")
+
+z = requests.get("https://data.binance.com/api/v3/ticker/24hr")
+
+a = z.json()
+
+low_price = []
+
+for i in a:
+    low_price.append(i['lowPrice'])
+
+plt.figure()
+plt.scatter()
+plt.show()
+
